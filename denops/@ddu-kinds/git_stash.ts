4f2b10f8a483f4160cb69509a14a48a8aba0ae69
Peacock <contact@peacock0803sz.com>
2023-08-01 21:02:09 +0900

    :recycle: Resolve type puzzules

--- denops/@ddu-kinds/git_stash.ts
+++ denops/@ddu-kinds/git_stash.ts
@@ -6,6 +6,7 @@ import {
   DduItem,
   Previewer,
 } from "https://deno.land/x/ddu_vim@v3.4.4/types.ts";
+import { Denops } from "https://deno.land/x/denops_core@v5.0.0/mod.ts";
 
 type Params = Record<string, unknown>;
 type Never = Record<never, never>;
@@ -16,17 +17,33 @@ export type ActionData = {
   message: string;
 };
 
+async function ensureFirstItem(denops: Denops, items: DduItem[]) {
+  if (items.length != 1) {
+    await denops.call(
+      "ddu#util#print_error",
+      "invalid action calling: it can accept only one item",
+      "ddu-kind-git_stash",
+    );
+    return;
+  }
+  return items[0];
+}
+
 const getCwd = (item: DduItem): string => {
   return (item.action as ActionData).cwd;
 };
 
+const getHash = (item: DduItem): string => {
+  return (item.action as ActionData).label;
+}
+
 const executeGit = (
   args: string[],
-  items: DduItem[],
+  item: DduItem,
 ): Promise<Deno.CommandOutput> => {
   const cmd = new Deno.Command("git", {
-    args: args,
-    cwd: getCwd(items[0]),
+    args: [...args, getHash(item)],
+    cwd: getCwd(item),
   });
   return cmd.output();
 };
@@ -37,25 +54,29 @@ export class Kind extends BaseKind<Params> {
     (args: ActionArguments<Never>) => Promise<ActionFlags>
   > = {
     apply: async (args) => {
-      const label: string = args.items[0].action?.label; // FIXME: type puzzle
-      await executeGit(["stash", "apply", label], args.items);
+      const item = await ensureFirstItem(args.denops, args.items);
+      if (!item) return ActionFlags.None;
+      await executeGit(["stash", "apply"], item);
       return ActionFlags.RefreshItems;
     },
     pop: async (args) => {
-      const label: string = args.items[0].action?.label; // FIXME: type puzzle
-      await executeGit(["stash", "pop", label], args.items);
+      const item = await ensureFirstItem(args.denops, args.items);
+      if (!item) return ActionFlags.None;
+      await executeGit(["stash", "pop"], item);
       return ActionFlags.RefreshItems;
     },
     drop: async (args) => {
-      const label: string = args.items[0].action?.label; // FIXME: type puzzle
-      await executeGit(["stash", "drop", label], args.items);
+      const item = await ensureFirstItem(args.denops, args.items);
+      if (!item) return ActionFlags.None;
+      await executeGit(["stash", "drop"], item);
       return ActionFlags.RefreshItems;
     },
     clear: async (args) => {
-      const label: string = args.items[0].action?.label; // FIXME: type puzzle
-      await executeGit(["stash", "clear", label], args.items);
+      const item = await ensureFirstItem(args.denops, args.items);
+      if (!item) return ActionFlags.None;
+      await executeGit(["stash", "drop"], item);
       return ActionFlags.RefreshItems;
-    }
+    },
   };
 
   getPreviewer(args: GetPreviewerArguments): Promise<Previewer | undefined> {
